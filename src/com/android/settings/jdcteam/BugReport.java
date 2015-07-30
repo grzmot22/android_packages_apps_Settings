c0346f228b02932c674db14ce36c7b489e5989e0
Mattia D'Alleva <antaresone@antaresone.eu>
2015-07-30 21:47:11 +0200

    settings: enhanced MSS
    Suggested by smstiv@XDA
    
    10 clicks needed to show content

--- src/com/android/settings/jdcteam/BugReport.java
+++ src/com/android/settings/jdcteam/BugReport.java
@@ -1,7 +1,10 @@
 package com.android.settings.jdcteam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.Preference;
+import android.widget.Toast;
 
 import com.android.settings.R;
 
@@ -10,9 +13,31 @@ import com.android.settings.R;
  */
 public class BugReport extends PreferenceActivity {
 
+    int taps = 0;
+    Intent start;
+    Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.bug_report_prefs);
+        Preference mss = findPreference ("mss");
+        mss.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                taps = taps+1;
+                switch ( taps ) {
+                    case 7:
+                        toast = new Toast(BugReport.this);
+                        toast.makeText(BugReport.this, R.string.missing_clicks, Toast.LENGHT_SHORT).show();
+                        break;
+                    case 10:
+                        start = new Intent (BugReport.this, WeirdShit.class);
+                        startActivity(start);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
