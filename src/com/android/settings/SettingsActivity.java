3b9aa2442a06454bc0de55d3379eaca13c8901ad
grzmot22 <grzmot22@gmail.com>
2016-01-23 20:09:49 +0100

    Fix 2
    Change-Id: Ibe535847d0fcca2c174c9f3078bbb92114de11c6

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1327,8 +1327,6 @@ public class SettingsActivity extends Activity
                     boolean hasDeviceKeys = getResources().getInteger(
                             com.android.internal.R.integer.config_deviceHardwareKeys) != 0;
                     if (!hasDeviceKeys) {
-						removeTile = true
-                    }
                 } else if (id == R.id.ota_settings) {
                     // Embedding into Settings only if app exists (user could manually remove it)
                     boolean supported = false;
